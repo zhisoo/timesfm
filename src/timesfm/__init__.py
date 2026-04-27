7fa770236807bfbd9df1d4301a881114d5df7aa2
Zhi Soo <zhisoo@users.noreply.github.com>
2026-04-27 17:53:58 +0800

    feat: add package __init__.py with public API exports

--- src/timesfm/__init__.py
+++ src/timesfm/__init__.py
@@ -1,4 +1,4 @@
-# Copyright 2025 Google LLC
+# Copyright 2024 The TimesFM Authors.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,18 +12,32 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""TimesFM API."""
+"""TimesFM: A time series foundation model for forecasting.
 
-from .configs import ForecastConfig
+This package provides a fork of Google Research's TimesFM model,
+enabling zero-shot time series forecasting with a pre-trained
+foundation model.
 
-try:
-  from .timesfm_2p5 import timesfm_2p5_torch
-  TimesFM_2p5_200M_torch = timesfm_2p5_torch.TimesFM_2p5_200M_torch
-except ImportError:
-  pass
+Basic usage:
+    >>> import timesfm
+    >>> tfm = timesfm.TimesFm(
+    ...     context_len=512,
+    ...     horizon_len=96,
+    ...     backend="cpu",
+    ... )
+    >>> tfm.load_from_checkpoint(repo_id="google/timesfm-1.0-200m")
+    >>> forecast = tfm.forecast(inputs, freq=[0])
+"""
 
-try:
-  from .timesfm_2p5 import timesfm_2p5_flax
-  TimesFM_2p5_200M_flax = timesfm_2p5_flax.TimesFM_2p5_200M_flax
-except ImportError:
-  pass
+from timesfm.timesfm_base import TimesFmBase
+from timesfm.timesfm_torch import TimesFm
+from timesfm import data_loader
+
+__version__ = "1.0.0"
+__author__ = "TimesFM Contributors"
+
+__all__ = [
+    "TimesFm",
+    "TimesFmBase",
+    "data_loader",
+]
